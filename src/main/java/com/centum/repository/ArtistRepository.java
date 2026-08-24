4bb6d56c279ee4ef6dae7123fc2b20b829c12f48
Carol Nunes <carolnunes025@gmail.com>
2026-08-24 14:05:04 -0300

    repositorio entidades1

---
+++ src/main/java/com/centum/repository/ArtistRepository.java
@@ -0,0 +1,9 @@
+package com.centum.repository;
+
+import com.centum.entity.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
+    Optional<Artist> findBySlug(String slug);
+}
